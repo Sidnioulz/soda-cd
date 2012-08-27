53a3a7493837a2bd70cdf0b1249cb3b9179ceab0
Steve Dodier-Lazaro <steve.dodier-lazaro@inria.fr>
2012-08-27 10:51:11 +0200

    Modified doc index page
    original bzr status:
    revno: 50
    committer: Steve Dodier-Lazaro <steve.dodier-lazaro@inria.fr>
    branch nick: trunk
    timestamp: Mon 2012-08-27 10:51:11 +0200

--- src/main.h
+++ src/main.h
@@ -37,7 +37,7 @@
  *
  * \section intro_sec About this application.
  *
- * PEPSI's has been developed as part of a research internship in the <a href="http://www.inria.fr/equipes/vr4i">Inria VR4I</a> project team, by Steve Dodier-Lazaro, under the supervision of Valérie Gouranton and Quentin Avril. It is currently not available for download and is the exclusive property of Inria Rennes.
+ * PEPSI's has been developed as part of a research internship in the <a href="http://www.inria.fr/equipes/vr4i">IRISA-Inria VR4i</a> project team in Rennes, France. The main developer is Steve Dodier-Lazaro, who worked under the supervision of Valérie Gouranton and Quentin Avril. It is currently not available for download and is the exclusive property of Inria Rennes, but a release of a final prototype will be made if given the occasion.
  *
  * \section install_sec Building PEPSI's
  *
@@ -46,8 +46,11 @@
  *
  * Note that the Blitz++ Fedora 16 package misses some code headers, and you may need to manually install them to build PEPSI's.
  *
+ * \section status_sec Current status
+ * There are currently no active developers for PEPSI's. It is at the moment capable of detecting collisions in a distributed fashion, but constraint solving between objects managed by different processors has not yet been implemented, and a few bugs remain to be fixed. Parallel/Distributed constraint solving may be more efficient/clever with a different granularity than the one chosen in PEPSI's and version 1 of the SODA framework, and a proposal will be issued for the development of this component.
+ *
  * \section report_bugs Reporting a bug
- * Please email either <a href="mailto:steve.dodier-lazaro@inria.fr">Steve</a> or <a href="mailto:quentin.avril@irisa.fr">Quentin</a>.
+ * If you are in-house, please write a bug report in the Inria GForge page of the project (https://gforge.inria.fr/tracker/?atid=13788&group_id=4142&func=browse), or email <a href="mailto:valerie.gouranton@inria.fr">the project head</a> in last resort for her to dispatch the bug report to current developers (specifying [SODA-BUGS] at the start of the mail subject). Steve has the technical know-how about the project and can be reached at <a href="mailto:steve.dodier@ensi-bourges.fr">his school mail address</a>. Same mail subject rules apply.
  */
 
 //TODO: when objects and cells too big to populate several worlds, don't!
